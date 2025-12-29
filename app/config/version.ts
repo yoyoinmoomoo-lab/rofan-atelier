02df47a35c4a87e5c7dc67defd24159d732f4515
박선하 <sunhapark@bagseonhaui-noteubug.local>
2025-12-29 13:32:08 +0900

    chore: bump version to 1.2.2

--- app/config/version.ts
+++ app/config/version.ts
@@ -1,8 +1,8 @@
 // rofan.world version
-export const ROFAN_WORLD_VERSION = "1.2.1";
+export const ROFAN_WORLD_VERSION = "1.2.2";
 
-// Rofan Visualboard version (for display in test-board)
+// Vivid Chat version (for display in test-board)
 export const VISUALBOARD_VERSION = "0.2.0";
 
-// Compatible Visualboard version
+// Compatible Vivid Chat version
 export const COMPATIBLE_VISUALBOARD_VERSION = "0.2.0+";
